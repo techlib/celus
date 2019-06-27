6af659fa96d8385bf6f6eecc94f416f44381f99e
Beda Kosata <beda@bigdigdata.com>
2019-06-27 08:02:31 +0200

    make it possible to use chart-data api view for data without date as primary dimension

--- apps/logs/serializers.py
+++ apps/logs/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Metric
+from .models import Metric, Dimension
 
 
 class MetricSerializer(ModelSerializer):
@@ -8,3 +8,10 @@ class MetricSerializer(ModelSerializer):
     class Meta:
         model = Metric
         fields = ('pk', 'short_name', 'name')
+
+
+class DimensionSerializer(ModelSerializer):
+
+    class Meta:
+        model = Dimension
+        fields = ('pk', 'short_name', 'name', 'type')
--- apps/logs/views.py
+++ apps/logs/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from logs.logic.remap import remap_dicts
 from logs.models import Metric, AccessLog, ReportType, Dimension
-from logs.serializers import MetricSerializer
+from logs.serializers import MetricSerializer, DimensionSerializer
 from organizations.models import Organization
 from organizations.serializers import OrganizationSerializer
 from publications.models import Platform
@@ -20,29 +20,40 @@ class Counter5DataView(APIView):
         platform = get_object_or_404(Platform, pk=request.GET.get('platform'))
         report_type = get_object_or_404(ReportType, short_name=request.GET.get('report_type'))
         secondary_dim = request.GET.get('sec_dim')
-        primary_dim = 'date'
+        primary_dim = request.GET.get('prim_dim', 'date')
         query = AccessLog.objects.filter(
             source__organization=organization,
             source__platform=platform,
             metric=metric,
         )
+        prim_dim_obj = None
+        if primary_dim != 'date':
+            prim_dim_idx = int(primary_dim)
+            prim_dim_obj = report_type.dimensions_sorted[prim_dim_idx-1]
+            prim_dim_name = f'dim{prim_dim_idx}'
+        else:
+            prim_dim_name = primary_dim
+
         if secondary_dim:
             dim_idx = int(secondary_dim)
             dim_obj = report_type.dimensions_sorted[dim_idx-1]
-            print(dim_obj)
             dim_name = f'dim{dim_idx}'
-            data = query.values(primary_dim, dim_name).annotate(count=Sum('value')).\
-                values(primary_dim, 'count', dim_name).order_by(primary_dim, dim_name)
+            data = query.values(prim_dim_name, dim_name).annotate(count=Sum('value')).\
+                values(prim_dim_name, 'count', dim_name).order_by(prim_dim_name, dim_name)
             if dim_obj.type == Dimension.TYPE_TEXT:
                 remap_dicts(dim_obj, data, dim_name)
         else:
-            data = query.values(primary_dim).annotate(count=Sum('value')).\
-                values(primary_dim, 'count').order_by(primary_dim)
+            dim_obj = None
+            data = query.values(prim_dim_name).annotate(count=Sum('value')).\
+                values(prim_dim_name, 'count').order_by(prim_dim_name)
+        if primary_dim != 'date' and prim_dim_obj.type == Dimension.TYPE_TEXT:
+            remap_dicts(prim_dim_obj, data, prim_dim_name)
         reply = {
             'data': data,
             'organization': OrganizationSerializer(organization).data,
             'metric': MetricSerializer(metric).data,
             'platform': PlatformSerializer(platform).data,
-            'sec_dim': None if not secondary_dim else secondary_dim,
+            'sec_dim': DimensionSerializer(dim_obj).data if secondary_dim else None,
+            'prim_dim': DimensionSerializer(prim_dim_obj).data if prim_dim_obj else primary_dim,
         }
         return Response(reply)
