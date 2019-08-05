1fa3a52efd6f998a909e44a8d11016d2fa9e13cd
Beda Kosata <beda@bigdigdata.com>
2019-08-05 08:23:23 +0200

    small code clenup

--- design/ui/src/pages/AfterLoginPage.vue
+++ design/ui/src/pages/AfterLoginPage.vue
@@ -1,22 +1,5 @@
 <template>
-    <!--v-layout justify-center>
-        <div v-if="!user" class="text-center ma-12">
-            <v-progress-circular
-                    indeterminate
-                    size="100"
-            ></v-progress-circular>
-        </div>
-        <v-dialog persistent max-width="290">
-            <v-card>
-                <v-card-title class="headline">Still not logged in</v-card-title>
-                <v-card-text>You are not logged in. Please click Login to be redirected to login page.</v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" text href="/secure">Login</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </v-layout-->
+    <div></div>
 </template>
 
 <script>
