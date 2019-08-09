07b7b0919c7b939202a3fe8f94660c4df357aa32
Beda Kosata <beda@bigdigdata.com>
2019-08-09 07:40:16 +0200

    add filtering to list of sushi credentials - refs #9

--- design/ui/src/pages/SushiCredentialsManagementPage.vue
+++ design/ui/src/pages/SushiCredentialsManagementPage.vue
@@ -2,48 +2,69 @@
 
 <template>
     <v-layout>
-        <v-data-table
-                :items="sushiCredentialsList"
-                :headers="headers"
-
-        >
-             <template v-slot:items="props">
-                 <td>{{ props.item.organization.name }}</td>
-                 <td>{{ props.item.platform.name }}</td>
-                 <td>
-                     <v-chip
-                             v-for="(report, index) in props.item.active_counter_reports"
-                             class="ma-1"
-                             color="teal"
-                             text-color="white"
-                             :key="index"
-                     >
-                         <v-avatar left color="white">
-                             <span class="teal--text">{{ report.counter_version }}</span>
-                         </v-avatar>
-                         {{ report.code }}
-                     </v-chip>
-                 </td>
-                 <td>
-                     <v-btn flat small color="secondary">
-                         <v-icon left>fa-edit</v-icon>
-                         {{ $t('edit') }}
-                     </v-btn>
-                 </td>
-            </template>
-        </v-data-table>
+        <v-card>
+            <v-card-title>
+                <v-spacer></v-spacer>
+                <v-text-field
+                        v-model="searchDebounced"
+                        append-icon="fa-search"
+                        :label="$t('labels.search')"
+                        single-line
+                        hide-details
+                        clearable
+                        clear-icon="fa-times"
+                ></v-text-field>
+            </v-card-title>
+            <v-data-table
+                    :items="sushiCredentialsList"
+                    :headers="headers"
+                    :search="search"
+                    :pagination.sync="pagination"
+            >
+                <template v-slot:items="props">
+                    <td>{{ props.item.organization.name }}</td>
+                    <td>{{ props.item.platform.name }}</td>
+                    <td>
+                        <v-chip
+                                v-for="(report, index) in props.item.active_counter_reports"
+                                class="ma-1"
+                                color="teal"
+                                text-color="white"
+                                :key="index"
+                        >
+                            <v-avatar left color="white">
+                                <span class="teal--text">{{ report.counter_version }}</span>
+                            </v-avatar>
+                            {{ report.code }}
+                        </v-chip>
+                    </td>
+                    <td>
+                        <v-btn flat small color="secondary">
+                            <v-icon left>fa-edit</v-icon>
+                            {{ $t('edit') }}
+                        </v-btn>
+                    </td>
+                </template>
+            </v-data-table>
+        </v-card>
     </v-layout>
 </template>
 
 <script>
   import axios from 'axios'
   import {mapActions} from 'vuex'
+  import { debounce } from 'lodash'
 
   export default {
     name: "SushiCredentialsManagementPage",
     data () {
       return {
-        'sushiCredentialsList': [],
+        sushiCredentialsList: [],
+        search: '',
+        pagination: {
+          sortBy: 'name',
+          rowsPerPage: 25,
+        },
       }
     },
     computed: {
@@ -58,12 +79,25 @@
             value: 'platform.name'
           },
           {
+            text: this.$i18n.t('title_fields.active_reports'),
+            value: '',
+            sortable: false,
+          },
+          {
             text: this.$i18n.t('title_fields.actions'),
             value: '',
             sortable: false,
           },
         ]
       },
+      searchDebounced: {
+        get () {
+          return this.search
+        },
+        set: debounce(function (value) {
+          this.search = value
+        }, 500)
+      }
     },
     methods: {
       ...mapActions({
@@ -85,6 +119,16 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss">
+
+    button.v-btn {
+        i.fa {
+            font-size: 100%;
+
+            &.v-icon--left {
+                margin-right: 8px
+            }
+        }
+    }
 
 </style>
