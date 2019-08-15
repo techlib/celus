4f11422a42579dd8cf55aa6723fbc6f971406162
Beda Kosata <beda@bigdigdata.com>
2019-08-15 11:57:04 +0200

    add online ERMS sync when authenticating user

--- apps/core/auth.py
+++ apps/core/auth.py
@@ -1,4 +1,30 @@
-from core.models import Identity, User
+import logging
+
+from django.conf import settings
+
+from core.logic.sync import UserSyncer, IdentitySyncer
+from core.models import Identity, User, DataSource
+from erms.api import ERMS
+
+
+logger = logging.getLogger(__name__)
+
+
+def sync_user_with_erms(user_id):
+    erms = ERMS(settings.ERMS_API_URL)
+    identities = erms.fetch_endpoint(ERMS.EP_IDENTITY, identity=user_id)
+    if identities:
+        identity = identities[0]
+        persons = erms.fetch_objects(ERMS.CLS_PERSON, object_id=identity['person'])
+        if persons:
+            data_source, _created = DataSource.objects.get_or_create(short_name='ERMS',
+                                                                     type=DataSource.TYPE_API)
+            u_syncer = UserSyncer(data_source)
+            stats = u_syncer.sync_data(persons)
+            logger.debug('User sync on login: %s', stats)
+            i_syncer = IdentitySyncer(data_source)
+            stats = i_syncer.sync_data(identities)
+            logger.debug('Identity sync on login: %s', stats)
 
 
 class EDUIdAuthenticationBackend:
@@ -11,6 +37,10 @@ class EDUIdAuthenticationBackend:
     def authenticate(self, request, remote_user=None):
         if not remote_user:
             return None
+        try:
+            sync_user_with_erms(remote_user)
+        except Exception as e:
+            logger.error('ERMS sync error:', e)
         try:
             identity = Identity.objects.select_related('user').get(identity=remote_user)
         except Identity.DoesNotExist:
