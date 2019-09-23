db4a2080807f94f283ad018a4850589c82e0d01a
Beda Kosata <beda@bigdigdata.com>
2019-09-23 22:22:01 +0200

    catch blocking timeout exception in cache_based_lock

--- apps/core/task_support.py
+++ apps/core/task_support.py
@@ -3,7 +3,7 @@ import logging
 from time import sleep, time
 
 from django.core.cache import cache
-
+from redis.exceptions import LockError
 
 logger = logging.getLogger(__name__)
 
@@ -11,14 +11,25 @@ logger = logging.getLogger(__name__)
 @contextmanager
 def cache_based_lock(lock_name, timeout=3600, blocking_timeout=None):
     if hasattr(cache, 'lock'):
-        with cache.lock(lock_name, timeout=timeout, blocking_timeout=blocking_timeout):
-            yield None
+        try:
+            with cache.lock(lock_name, timeout=timeout, blocking_timeout=blocking_timeout):
+                yield None
+        except LockError as e:
+            # we ignore error about not blocking timeout
+            if e.args == ('Unable to acquire lock within the time specified',):
+                pass
+            else:
+                raise e
     else:
         # cache does not support locking, we use a poor mans solution
         is_free = cache.add(lock_name, 1, timeout)
+        start_time = time()
         if not is_free:
             logger.warning("Task is locked. Waiting for lock release of '{}'".format(lock_name))
         while not is_free:
+            if blocking_timeout and time() - start_time > blocking_timeout:
+                yield None
+                return
             sleep(0.5)
             is_free = cache.add(lock_name, 1, timeout)
         logger.info("Task is free to run")
