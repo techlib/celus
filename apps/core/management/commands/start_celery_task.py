3b6ac216cc2d1c7b299c3fcc24b5d29927c0ddfc
Beda Kosata <beda@bigdigdata.com>
2019-09-23 10:26:43 +0200

    add management command to start celery tasks

---
+++ apps/core/management/commands/start_celery_task.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+
+from sushi.tasks import fetch_new_sushi_data_task
+
+
+class Command(BaseCommand):
+
+    help = 'Start the specified celery task'
+
+    tasks = {
+        'fetch_new_sushi_data_task': fetch_new_sushi_data_task,
+    }
+
+    def add_arguments(self, parser):
+        parser.add_argument('task')
+
+    def handle(self, *args, **options):
+        task_name = options['task']
+        task = self.tasks.get(task_name)
+        if not task:
+            self.stderr.write(self.style.ERROR(f'Cannot find task: {task_name}'))
+            self.stderr.write(self.style.WARNING(
+                'Available tasks: {}'.format(', '.join(self.tasks.keys()))))
+        else:
+            self.stderr.write(self.style.SUCCESS(f'Starting task: {task_name}'))
+            handle = task.delay()
+            self.stderr.write(self.style.SUCCESS(f'Task handle: {handle}'))
+
+
