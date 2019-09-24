a3f967e903975c88abdef9caa8107da1defcd46d
Beda Kosata <beda@bigdigdata.com>
2019-09-24 07:52:49 +0200

    use LockError to properly dispose of task with blocking timeout

--- apps/core/task_support.py
+++ apps/core/task_support.py
@@ -11,15 +11,8 @@ logger = logging.getLogger(__name__)
 @contextmanager
 def cache_based_lock(lock_name, timeout=3600, blocking_timeout=None):
     if hasattr(cache, 'lock'):
-        try:
-            with cache.lock(lock_name, timeout=timeout, blocking_timeout=blocking_timeout):
-                yield None
-        except LockError as e:
-            # we ignore error about not blocking timeout
-            if e.args == ('Unable to acquire lock within the time specified',):
-                yield None
-            else:
-                raise e
+        with cache.lock(lock_name, timeout=timeout, blocking_timeout=blocking_timeout):
+            yield None
     else:
         # cache does not support locking, we use a poor mans solution
         is_free = cache.add(lock_name, 1, timeout)
@@ -28,8 +21,7 @@ def cache_based_lock(lock_name, timeout=3600, blocking_timeout=None):
             logger.warning("Task is locked. Waiting for lock release of '{}'".format(lock_name))
         while not is_free:
             if blocking_timeout and time() - start_time > blocking_timeout:
-                yield None
-                return
+                raise LockError('Blocking timeout')
             sleep(0.5)
             is_free = cache.add(lock_name, 1, timeout)
         logger.info("Task is free to run")
