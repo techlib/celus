b57f62f0df9e8c388e8f35c05cccfc82951782d7
Beda Kosata <beda@bigdigdata.com>
2020-01-09 09:11:53 +0100

    add admin action to delete sushi fetch attempts with associated import batches; add wrapping to sushi fetch attempt django admin table header

--- apps/sushi/admin.py
+++ apps/sushi/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin, messages
+from django.db.transaction import atomic
 
 from reversion.admin import VersionAdmin
 
 from logs.logic.attempt_import import reprocess_attempt
+from logs.models import ImportBatch
 from . import models
 
 
@@ -38,6 +40,19 @@ def reimport(modeladmin, request, queryset):
 reimport.short_description = 'Reimport data - deletes old and reparses the downloaded file'
 
 
+@atomic
+def delete_with_data(modeladmin, request, queryset):
+    batches_deleted = ImportBatch.objects.filter(
+        pk__in=queryset.filter(import_batch__isnull=False).values('import_batch_id')
+    ).delete()
+    attempts_deleted = queryset.delete()
+    messages.info(request, f'{attempts_deleted[0]} attempts deleted with '
+                           f'{batches_deleted[0]} data batches')
+
+
+delete_with_data.short_description = 'Delete selected attempts including related usage data'
+
+
 class HasImportBatch(admin.SimpleListFilter):
     title = 'has import batch'
     parameter_name = 'has'
@@ -61,17 +76,23 @@ class SushiFetchAttemptAdmin(admin.ModelAdmin):
 
     list_display = ['organization', 'platform', 'counter_version', 'report', 'timestamp',
                     'start_date', 'end_date',
-                    'queued', 'import_crashed', 'download_success', 'processing_success',
-                    'is_processed', 'contains_data', 'error_code', 'has_import_batch']
+                    'queued', 'download_success', 'processing_success',
+                    'contains_data', 'import_crashed', 'is_processed', 'error_code',
+                    'has_import_batch']
     list_filter = ['download_success',  'processing_success', 'is_processed', 'queued',
                    'import_crashed', 'error_code', 'contains_data', HasImportBatch,
                    'counter_report', 'credentials__organization', 'credentials__platform']
     readonly_fields = ['credentials', 'counter_report', 'timestamp', 'start_date', 'end_date',
                        'download_success', 'data_file', 'import_batch', 'queue_previous']
     search_fields = ['credentials__organization__name', 'credentials__platform__name', 'pk']
-    actions = [reimport]
+    actions = [delete_with_data, reimport]
     list_select_related = ['credentials__platform', 'credentials__organization', 'credentials',
-                           'counter_report']
+                           'counter_report', 'import_batch']
+
+    class Media:
+        css = {
+            "all": ("admin/admin.css",)
+        }
 
     def report(self, obj: models.SushiFetchAttempt):
         return obj.counter_report.code
