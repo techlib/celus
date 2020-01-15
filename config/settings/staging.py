b6ba84cc36bc5614161519e16e0bca4dd0769fb5
Beda Kosata <beda@bigdigdata.com>
2020-01-15 16:53:28 +0100

    add staging configuration

---
+++ config/settings/staging.py
@@ -0,0 +1,16 @@
+from .base import *
+
+ALLOWED_HOSTS = ['stats.test.czechelib.cz']
+LIVE_ERMS_AUTHENTICATION = True
+
+DEBUG = False
+
+STATIC_ROOT = '/var/www/html/stats/static/'
+MEDIA_ROOT = '/var/www/html/stats/media/'
+
+ADMINS = (
+    ("Beda Kosata", "beda@bigdigdata.com"),
+)
+
+EMAIL_HOST = 'smtp.ntkcz.cz'
+SERVER_EMAIL = 'admin@stats.test.czechelib.cz'
