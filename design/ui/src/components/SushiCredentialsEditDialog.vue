d665873fc842be5cc94f77db115ec34239220513
Beda Kosata <beda@bigdigdata.com>
2020-01-20 08:57:18 +0100

    update sushi credentials edit dialog to new vuetify style

--- design/ui/src/components/SushiCredentialsEditDialog.vue
+++ design/ui/src/components/SushiCredentialsEditDialog.vue
@@ -36,165 +36,163 @@ cs:
     <v-card>
         <v-card-title class="headline">{{ $t('title.edit_sushi_credentials') }}</v-card-title>
         <v-card-text>
-            <v-layout row pa-4>
-                <v-flex xs12 px-4>
-                    <v-layout row>
-                        <v-flex xs12 md6 px-5>
-                            <v-text-field
-                                    v-if="credentials"
-                                    v-model="organization"
-                                    :label="$t('organization')"
-                                    disabled
-                            >
-                            </v-text-field>
-                            <v-select v-else
-                                      v-model="organization"
-                                      :items="organizations"
-                                      item-text="name"
-                                      :label="$t('organization')"
-                                      return-object
-                                      :disabled="organizationSelected"
-                            >
-                            </v-select>
-                        </v-flex>
-                        <v-flex xs12 md6 px-5>
-                            <v-text-field
-                                    v-if="credentials"
-                                    v-model="platform"
-                                    :label="$t('platform')"
-                                    disabled
-                            >
-                            </v-text-field>
-                            <v-select v-else
-                                      v-model="platform"
-                                      :items="allowedPlatforms"
-                                      item-text="short_name"
-                                      :label="$t('platform')"
-                                      return-object
-                            >
-                            </v-select>
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-                <v-flex xs12 pt-5 px-4>
-                    <v-layout row>
-                        <v-flex xs12 sm6 px-5>
-                            <v-text-field
-                                    v-model="requestorId"
-                                    :label="$t('labels.requestor_id')"
-                            >
-                            </v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 px-5>
-                            <v-text-field
-                                    v-model="customerId"
-                                    :label="$t('labels.customer_id')"
-                            >
-                            </v-text-field>
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-                <v-flex xs12 pt-3 px-4>
-                    <v-layout row>
-                        <v-flex xs6 sm2 px-5>
-                            <v-select
-                                    v-model="counterVersion"
-                                    :label="$t('labels.counter_version')"
-                                    :items="allowedCounterVersions"
-                                    :disabled="!!credentials"
-                                    :no-data-text="$t('all_versions_used')"
-                            >
-                            </v-select>
-                        </v-flex>
-                        <v-flex xs12 sm10 px-5>
-                            <v-text-field
-                                    v-model="url"
-                                    :label="$t('labels.url')"
-                                    :error-messages="errors.url"
-                            >
-                            </v-text-field>
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-                <v-flex xs12>
-                    <v-expansion-panels class="px-2 pt-4">
-                        <v-expansion-panel>
-                            <v-expansion-panel-header>{{ $t('labels.http_authentication') }}</v-expansion-panel-header>
-                            <v-expansion-panel-content>
-                                <v-layout row>
-                                    <v-flex xs12 sm6 px-5>
-                                        <v-text-field
-                                                v-model="httpUsername"
-                                                :label="$t('labels.http_username')"
-                                        >
-                                        </v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12 sm6 px-5>
-                                        <v-text-field
-                                                v-model="httpPassword"
-                                                :label="$t('labels.http_password')"
-                                        >
-                                        </v-text-field>
-                                    </v-flex>
-                                </v-layout>
-                            </v-expansion-panel-content>
-                        </v-expansion-panel>
-                        <v-expansion-panel>
-                            <v-expansion-panel-header>{{ $t('labels.extra_params') }}</v-expansion-panel-header>
-                            <v-expansion-panel-content>
-                                <v-layout row>
-                                    <v-flex xs12 px-5>
-                                        <v-text-field
-                                                v-model="apiKey"
-                                                :label="$t('labels.api_key')"
-                                        >
-                                        </v-text-field>
-                                    </v-flex>
-                                </v-layout>
-                                <v-layout row v-for="(param, index) in extraParams" :key="index">
-                                    <v-flex xs12 sm6 px-5>
-                                        <v-text-field
-                                                v-model="param.key"
-                                                :label="$t('labels.variable')"
-                                        >
-                                        </v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12 sm6 px-5>
-                                        <v-text-field
-                                                v-model="param.value"
-                                                :label="$t('labels.variable_value')"
-                                        >
-                                        </v-text-field>
-                                    </v-flex>
-                                </v-layout>
-                                <v-layout>
-                                    <v-flex xs12 px-2 pt-2>
-                                        <v-btn @click="extraParams.push({})" outlined color="green">
-                                            <v-icon left x-small>fa-plus</v-icon>
-                                            {{ $t('add_custom_param') }}
-                                        </v-btn>
-                                    </v-flex>
-                                </v-layout>
-                            </v-expansion-panel-content>
-                        </v-expansion-panel>
-                    </v-expansion-panels>
-                </v-flex>
-                <v-flex px-5 pt-6>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <h4>Active report types</h4>
-                        </v-flex>
-                        <v-flex xs12 pt-2>
-                            <v-btn-toggle multiple v-model="selectedReportTypes" >
-                                <v-btn v-for="report in reportTypes" :value="report.id" :key="report.id" outlined color="primary">
-                                    {{ report.code }}
-                                </v-btn>
-                            </v-btn-toggle>
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-            </v-layout>
+            <v-container fluid>
+                <v-row>
+                    <v-col cols="12" :md="6">
+                        <v-text-field
+                                v-if="credentials"
+                                v-model="organization"
+                                :label="$t('organization')"
+                                disabled
+                        >
+                        </v-text-field>
+                        <v-select v-else
+                                  v-model="organization"
+                                  :items="organizations"
+                                  item-text="name"
+                                  :label="$t('organization')"
+                                  return-object
+                                  :disabled="organizationSelected"
+                        >
+                        </v-select>
+                    </v-col>
+                    <v-col cols="12" :md="6">
+                        <v-text-field
+                                v-if="credentials"
+                                v-model="platform"
+                                :label="$t('platform')"
+                                disabled
+                        >
+                        </v-text-field>
+                        <v-select v-else
+                                  v-model="platform"
+                                  :items="allowedPlatforms"
+                                  item-text="short_name"
+                                  :label="$t('platform')"
+                                  return-object
+                        >
+                        </v-select>
+                    </v-col>
+                </v-row>
 
+                <v-row>
+                    <v-col cols="12" :sm="6">
+                        <v-text-field
+                                v-model="requestorId"
+                                :label="$t('labels.requestor_id')"
+                        >
+                        </v-text-field>
+                    </v-col>
+                    <v-col cols="12" :sm="6">
+                        <v-text-field
+                                v-model="customerId"
+                                :label="$t('labels.customer_id')"
+                        >
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="6" :sm="2">
+                        <v-select
+                                v-model="counterVersion"
+                                :label="$t('labels.counter_version')"
+                                :items="allowedCounterVersions"
+                                :disabled="!!credentials"
+                                :no-data-text="$t('all_versions_used')"
+                        >
+                        </v-select>
+                    </v-col>
+                    <v-col cols="12" :sm="10">
+                        <v-text-field
+                                v-model="url"
+                                :label="$t('labels.url')"
+                                :error-messages="errors.url"
+                        >
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="12">
+                        <v-expansion-panels class="px-2 pt-4">
+                    <v-expansion-panel>
+                        <v-expansion-panel-header>{{ $t('labels.http_authentication') }}</v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            <v-layout row>
+                                <v-flex xs12 sm6 px-5>
+                                    <v-text-field
+                                            v-model="httpUsername"
+                                            :label="$t('labels.http_username')"
+                                    >
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex xs12 sm6 px-5>
+                                    <v-text-field
+                                            v-model="httpPassword"
+                                            :label="$t('labels.http_password')"
+                                    >
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                    <v-expansion-panel>
+                        <v-expansion-panel-header>{{ $t('labels.extra_params') }}</v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            <v-layout row>
+                                <v-flex xs12 px-5>
+                                    <v-text-field
+                                            v-model="apiKey"
+                                            :label="$t('labels.api_key')"
+                                    >
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout row v-for="(param, index) in extraParams" :key="index">
+                                <v-flex xs12 sm6 px-5>
+                                    <v-text-field
+                                            v-model="param.key"
+                                            :label="$t('labels.variable')"
+                                    >
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex xs12 sm6 px-5>
+                                    <v-text-field
+                                            v-model="param.value"
+                                            :label="$t('labels.variable_value')"
+                                    >
+                                    </v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout>
+                                <v-flex xs12 px-2 pt-2>
+                                    <v-btn @click="extraParams.push({})" outlined color="green">
+                                        <v-icon left x-small>fa-plus</v-icon>
+                                        {{ $t('add_custom_param') }}
+                                    </v-btn>
+                                </v-flex>
+                            </v-layout>
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+                    </v-col>
+                </v-row>
+
+                <v-divider />
+                <v-row>
+                    <v-col cols="12" class="pt-4 pb-0">
+                        <h4>Active report types</h4>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-btn-toggle multiple v-model="selectedReportTypes" >
+                            <v-btn v-for="report in reportTypes" :value="report.id" :key="report.id" outlined color="primary">
+                                {{ report.code }}
+                            </v-btn>
+                        </v-btn-toggle>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-card-text>
         <v-card-actions>
             <v-container fluid mx-2>
