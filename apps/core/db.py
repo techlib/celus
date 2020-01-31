506ca329f3a54afc28f56fd5bb26e01496955fcc
Beda Kosata <beda@bigdigdata.com>
2020-01-31 14:24:03 +0100

    add forgotten module implementing the ilike sql command

---
+++ apps/core/db.py
@@ -0,0 +1,18 @@
+from django.db.models import Lookup, TextField, CharField
+
+
+@TextField.register_lookup
+@CharField.register_lookup
+class ILike(Lookup):
+    """
+    Custom lookup that uses ILIKE on Postgres which is much faster than the default approach
+    when using a TRGM index
+    """
+
+    lookup_name = 'ilike'
+
+    def as_sql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return f"{lhs} ILIKE CONCAT('%%', {rhs}, '%%')", params
