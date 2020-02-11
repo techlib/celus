0316b285f78217db3e953842483b7ca519edbedf
Beda Kosata <beda@bigdigdata.com>
2020-02-11 20:23:29 +0100

    faster raw data export

--- apps/logs/logic/export.py
+++ apps/logs/logic/export.py
@@ -11,7 +11,14 @@ from ..models import AccessLog, DimensionText, ReportType
 
 class CSVExporter(object):
 
-    implicit_dims = ['platform', 'metric', 'organization', 'target', 'report_type']
+    implicit_dims = {
+        'platform': 'name',
+        'metric': 'short_name',
+        'organization': 'name',
+        'target': 'name',
+        'report_type': 'short_name',
+        'date': None,
+    }
     title_attrs = ['isbn', 'issn', 'eissn']
 
     def export_raw_accesslogs_to_file(self, query_params: dict) -> str:
@@ -30,25 +37,31 @@ class CSVExporter(object):
                             ReportType.objects.filter(pk__in=queryset.distinct('report_type_id').
                                                       values('report_type_id'))}
         # get all field names for the CSV
-        field_names = self.implicit_dims + self.title_attrs
+        field_name_map = {(f'{dim}__{attr}' if attr else dim): dim
+                          for dim, attr in self.implicit_dims.items()}
+        field_name_map.update({f'target__{attr}': attr for attr in self.title_attrs})
+        field_names = list(field_name_map.values())
         for tr, dims in rt_to_dimensions.items():
-            field_names += [dim.short_name for dim in dims]
+            field_names += [dim.short_name for dim in dims if dim.short_name not in field_names]
         field_names.append('value')
+        # values that will be retrieved from the accesslogs
+        values = ['value', 'report_type_id']
+        values += list(field_name_map.keys())
+        values += [f'dim{i+1}' for i in range(7)]
         # crate the writer
         writer = csv.DictWriter(stream, field_names)
         writer.writeheader()
         # write the records
-        for al in queryset.select_related(*self.implicit_dims).iterator():  # type: AccessLog
-            record = {dim: str(getattr(al, dim)) for dim in self.implicit_dims}
-            record['value'] = al.value
-            for i, dim in enumerate(rt_to_dimensions[al.report_type_id]):
-                value = getattr(al, f'dim{i+1}')
+        for al in queryset.values(*values).iterator():  # type: dict
+            record = {attr_out: al.get(attr_in) for attr_in, attr_out in field_name_map.items()}
+            record['value'] = al['value']
+            record['date'] = al['date']
+            for i, dim in enumerate(rt_to_dimensions[al['report_type_id']]):
+                value = al.get(f'dim{i+1}')
                 if dim.type == dim.TYPE_TEXT:
                     record[dim.short_name] = text_id_to_text.get(value, value)
                 else:
                     record[dim.short_name] = value
-            if al.target:
-                for attr in self.title_attrs:
-                    record[attr] = getattr(al.target, attr)
+
             writer.writerow(record)
 
