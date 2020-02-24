712cdf0816b0350085a2800c1113954ef2572aa4
Beda Kosata <beda@bigdigdata.com>
2020-02-24 15:33:12 +0100

    reduce the number of celery beat tasks for the demo installation

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,3 +19,15 @@ ADMINS = (
 
 EMAIL_HOST = 'smtp.ntkcz.cz'
 SERVER_EMAIL = 'admin@stats.czechelib.cz'
+
+# reduce the amount of celery beat tasks for demo
+CELERY_BEAT_SCHEDULE = {
+    'smart_interest_sync_task': {
+        'task': 'logs.tasks.smart_interest_sync_task',
+        'schedule': schedule(run_every=timedelta(minutes=10)),
+    },
+    'import_new_sushi_attempts_task': {
+        'task': 'logs.tasks.import_new_sushi_attempts_task',
+        'schedule': schedule(run_every=timedelta(minutes=5)),
+    },
+}
