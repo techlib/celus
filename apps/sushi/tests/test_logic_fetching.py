cb6b464626e3ba28ff80cbc80053a6ca543d1eea
Beda Kosata <beda@bigdigdata.com>
2020-06-02 15:42:53 +0200

    add code to retry holes in data in case the credentials have been updated since the last attempt - refs #44

--- apps/sushi/tests/test_logic_fetching.py
+++ apps/sushi/tests/test_logic_fetching.py
@@ -8,8 +8,8 @@ from nigiri.client import Sushi5Client
 from nigiri.counter5 import Counter5ReportBase
 from publications.models import Platform
 from ..logic.data_import import import_sushi_credentials
-from ..logic.fetching import months_to_cover, find_holes_in_data
-from sushi.models import CounterReportType, SushiCredentials
+from ..logic.fetching import months_to_cover, find_holes_in_data, retry_holes_with_new_credentials
+from sushi.models import CounterReportType, SushiCredentials, SushiFetchAttempt
 from organizations.tests.conftest import organizations
 from publications.tests.conftest import platforms
 from logs.tests.conftest import report_type_nd
@@ -27,7 +27,6 @@ class TestHelperFunctions(object):
         assert months[-1] == month_start(month_start(today) - timedelta(days=15))
 
 
-@pytest.mark.now
 @pytest.mark.django_db
 class TestHoleFillingMachinery(object):
     def test_find_holes_in_data(self, settings, organizations, report_type_nd):
@@ -88,3 +87,46 @@ class TestHoleFillingMachinery(object):
         assert len(holes) == 2, 'nothing should change'
         assert holes[0].attempt_count == 1
 
+    def test_retry_holes_with_new_credentials(self, settings, organizations, report_type_nd):
+        """
+        Tests the `find_holes_in_data` function.
+        """
+        # set the date to 3 months before today
+        first_month = month_start(month_start(now().date()) - timedelta(days=80))
+        settings.SUSHI_ATTEMPT_LAST_DATE = first_month.isoformat()[:7]
+        # create all the prerequisites
+        data = [
+            {
+                'platform': 'XXX',
+                'organization': organizations[1].internal_id,
+                'customer_id': 'BBB',
+                'requestor_id': 'RRRX',
+                'URL': 'http://this.is/test/2',
+                'version': 5,
+                'extra_attrs': 'auth=un,pass;api_key=kekekeyyy;foo=bar',
+            },
+        ]
+        Platform.objects.create(short_name='XXX', name='XXXX', ext_id=10)
+        import_sushi_credentials(data)
+        assert SushiCredentials.objects.count() == 1
+        cr1 = SushiCredentials.objects.get()
+        cr1.create_sushi_client()
+        report = CounterReportType.objects.create(
+            code='tr', name='tr', counter_version=5, report_type=report_type_nd(0)
+        )
+        cr1.active_counter_reports.add(report)
+
+        def mock_get_report_data(*args, **kwargs):
+            return Counter5ReportBase()
+
+        Sushi5Client.get_report_data = mock_get_report_data
+        # test that find_holes_in_data returns the right number of records
+        holes = find_holes_in_data()
+        assert len(holes) == 3
+        # add an attempt and try again
+        assert SushiFetchAttempt.objects.count() == 0
+        stats = retry_holes_with_new_credentials()
+        print(stats)
+        assert SushiFetchAttempt.objects.count() == 3
+        holes = find_holes_in_data()
+        assert len(holes) == 0
