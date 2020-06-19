c6da7c226b1f80a4aef7a6ac3e969bf297e53fb0
Beda Kosata <beda@bigdigdata.com>
2020-06-19 17:55:42 +0200

    add forgotten allauth.socialaccount app

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_auth',
     'django.contrib.sites',
     'allauth',
+    'allauth.socialaccount',
     'allauth.account',
     'rest_auth.registration',
     'core.apps.CoreConfig',
