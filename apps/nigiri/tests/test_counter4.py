6c149206cb3ca0384a66df9441454af4d2703330
Stepan Henek <stepan@bigdigdata.com>
2020-07-02 15:21:41 +0200

    Add tests for extracting counter 4 errors

---
+++ apps/nigiri/tests/test_counter4.py
@@ -0,0 +1,29 @@
+import pytest
+
+from pathlib import Path
+
+from nigiri.client import Sushi4Client
+
+
+class TestErrors:
+    @pytest.mark.parametrize(
+        "filename,code",
+        (
+            # ("4_BR2_invalid_location1.xml", 1110),
+            ("4_BR2_not_supported1.xml", 3000),
+            ("4_BR2_service_not_available1.xml", 1000),
+            ("4_BR2_unauthorized1.xml", 2000),
+            ("4_BR2_unauthorized2.xml", 2010),
+        ),
+    )
+    def test_error_extraction(
+        self, filename, code,
+    ):
+
+        client = Sushi4Client("https://example.com/", "user")
+
+        with (Path(__file__).parent / "data/counter4/" / filename).open() as f:
+            errors = client.extract_errors_from_data(f.read())
+            assert len(errors) == 1
+            error = errors[0]
+            assert int(error.code) == code
