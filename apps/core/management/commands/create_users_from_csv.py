b2df75a490b13fc5809eb3716f0dccaa3d764ca5
Beda Kosata <beda@bigdigdata.com>
2020-08-12 17:48:08 +0200

    add management command to create users from a CSV table

---
+++ apps/core/management/commands/create_users_from_csv.py
@@ -0,0 +1,73 @@
+import csv
+import logging
+from collections import Counter
+from distutils.util import strtobool
+
+from django.core.management.base import BaseCommand
+from django.db.transaction import atomic
+
+from core.models import User
+from organizations.models import UserOrganization, Organization
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    help = 'Sync organizations between ERMS and the database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
+        parser.add_argument('--do-it', dest='doit', action='store_true')
+
+    @atomic
+    def handle(self, *args, **options):
+        stats = Counter()
+        with open(options['csv_file'], 'r') as infile:
+            reader = csv.DictReader(infile)
+            for row in reader:
+                email = row.get('email')
+                if not email:
+                    raise ValueError('Row "email" is required')
+                email = email.strip()
+                username = row.get('username', email.split('@')[0])
+                superuser = bool(strtobool(row.get('superuser', 'False')))
+                staff = strtobool(row.get('staff', 'False'))
+                name = row.get('name', '')
+                first_name = name.split()[0] if name else ''
+                last_name = name.split()[-1] if name else ''
+                user_params = {
+                    'username': username,
+                    'is_superuser': superuser,
+                    'is_staff': staff,
+                    'first_name': first_name,
+                    'last_name': last_name,
+                }
+                user, created = User.objects.update_or_create(email=email, defaults=user_params,)
+                if email.startswith('jmoi'):
+                    print('dfsd', user.is_superuser, user_params)
+                if created:
+                    stats['user_created'] += 1
+                    logger.debug('created user %s: %s', email, user_params)
+                else:
+                    stats['user_existed'] += 1
+                    logger.debug('updating user %s: %s', email, user_params)
+                org_name = row.get('organization')
+                if org_name:
+                    organization = Organization.objects.get(internal_id=org_name.strip())
+                    is_admin = strtobool(row.get('org_admin', 'False'))
+                    uo, created = UserOrganization.objects.update_or_create(
+                        user=user, organization=organization, defaults={'is_admin': is_admin}
+                    )
+                    if created:
+                        stats['user-org_created'] += 1
+                    else:
+                        stats['user-org_existed'] += 1
+                else:
+                    self.stderr.write(
+                        self.style.WARNING(f'No organization specified for user: {email}')
+                    )
+
+        self.stderr.write(self.style.WARNING(f'Import stats: {stats}'))
+        if not options['doit']:
+            raise ValueError('preventing db commit, use --do-it to really do it ;)')
