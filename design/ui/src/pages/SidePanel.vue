796d432bc3a8d12162ed1096a743166660af83e8
Beda Kosata <beda@bigdigdata.com>
2020-09-03 16:25:32 +0200

    hide the sushi downloads page from non admins - fixes #127

--- design/ui/src/pages/SidePanel.vue
+++ design/ui/src/pages/SidePanel.vue
@@ -153,7 +153,7 @@
                 { title: this.$t('pages.maintenance'), icon: 'fa fa-toolbox', linkTo: 'maintenance', show: this.showManagementStuff },
                 { title: this.$i18n.t('pages.sushi_monthly_overview'), icon: 'far fa-calendar-check', linkTo: 'sushi-monthly-overview', show: this.showAdminStuff },
                 { title: this.$i18n.t('pages.sushi_management'), icon: 'far fa-arrow-alt-circle-down', linkTo: 'sushi-credentials-list', show: this.showAdminStuff },
-                { title: this.$t('pages.sushi_fetch_attempts'), icon: 'fa-retweet', linkTo: 'sushi-fetch-attempts', show: this.showAdminStuff },
+                { title: this.$t('pages.sushi_fetch_attempts'), icon: 'fa-retweet', linkTo: 'sushi-fetch-attempts', show: this.showManagementStuff },
                 { title: this.$t('pages.manual_data_uploads'), icon: 'fa-upload', linkTo: 'manual-data-upload-list', show: this.showAdminStuff && this.allowManualDataUpload },
                 //{ title: this.$t('pages.import_batches'), icon: 'fa-file-import', linkTo: 'import-batch-list', show: this.showAdminStuff },
               ],
