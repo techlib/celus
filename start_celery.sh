b381e558e1caf459fd85c903413fe7c8ecfd26d6
Stepan Henek <stepan@bigdigdata.com>
2020-11-27 16:01:41 +0100

    Celery upgrade to 5.0.2

--- start_celery.sh
+++ start_celery.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-celery -A config -Q celery,interest,sushi,import,normal worker -l DEBUG
+celery -A config worker -Q celery,interest,sushi,import,normal -l DEBUG
--- start_celerybeat.sh
+++ start_celerybeat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-celery beat -A config -l DEBUG
+celery -A config beat -l DEBUG
