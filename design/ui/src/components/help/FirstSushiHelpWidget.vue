41959f589397a5e7d0b97c5d94f2bfae28d2e6e5
Stepan Henek <stepan@bigdigdata.com>
2021-12-13 14:28:29 +0100

    Add czech translation for FirstSushiHelpWidget

--- design/ui/src/components/help/FirstSushiHelpWidget.vue
+++ design/ui/src/components/help/FirstSushiHelpWidget.vue
@@ -1,13 +1,5 @@
 <i18n lang="yaml">
 en:
-  add_credentials: Add credentials
-  continue: Continue
-cs:
-  add_credentials: Přidat pověření
-  continue: Dále
-</i18n>
-<i18n lang="yaml">
-en:
   step1: Hello
   step1_text:
     Hello, it looks like you are new to Celus and there are no data in your account.
@@ -48,15 +40,72 @@ en:
   step5_text: |
     When you have tested that your credentials work, we recommend activating automated
     harvesting of data using the <strong>Automatic harvesting</strong> toggle. For such credentials,
-    Celus will automatically harvest data including past usage.
+    Celus will automatically harvest data from the last month.
   step5_text_2: |
     <em>Note</em>: Please note that Celus does not start harvesting data immediately, but
-    rather schedules periodic downloads for night hours when the traffic is lowest. Give it
+    rather schedules periodic downloads for night hours when the traffic is lower. Give it
     a day or two to fetch all your data.
   step6: Let's do it
   step6_text:
     OK, you should now know everything that is needed to get you started. Click the
     button below and add your first SUSHI credentials. Good luck ;)
+  add_credentials: Add credentials
+  continue: Continue
+
+cs:
+  step1: Ahoj
+  step1_text:
+    Ahoj, zdá se že právě začínáte používat Celus a váš účet neobsahuje žádná data.
+    Pomohu Vám s nastavení prvního SUSHI stahování - tím zjískáte nějaká data,
+    se kterými můžete dál pracovat a zárověň se naučíte, jak Celus funguje.
+  step2: SUSHI úvod
+  step2_text: |
+    Úkolem Celusu je získávat statistická data od vašich poskytovatelů prostřednictvím
+    tzv. SUSHI protokolu. Pro získání dat potřebujete znát adresu serveru a několik
+    identifikačních údajů, jako <strong>requestor ID</strong> a <strong>customer ID</strong>.
+  step2_text_2: Tyto informace můžete získat od vašeho poskytovatele.
+  step2_text_3:
+    Pro představu, co přesně bude potřebovat, takto vypadá dialog pro zadávání přístupových údajů
+    do SUSHI v Celusu.
+  step3: Dodatečné přístupové údaje
+  step3_text:
+    Někdy Vám poskytovatel předá víc přístupových údajů než dvojici customer ID
+    a requestor ID. V COUNTER 5 se často navíc vyskytuje <strong>API klíč</strong>,
+    v COUNTERu 4 to pak může být uživatelské jméno a heslo pro HTTP autentizaci.
+    Pokud Vám poskytovatel takové údaje dodá, nepanikařte - Celus s tím počítá a
+    dá Vám možnost tyto údaje vyplnit při registraci Vašich přístupových údajů
+    do SUSHI.
+  step3_text_2: |
+    <em>Pozn.</em>: Berte prosím v potaz, že někteří poskytovatelé povolují přístup
+    na svoje SUSHI servery pouze z určitých IP adres. Pokud tedy zjistíte, že
+    nemůžete stahovat data i přesto, že vaše přístupové údaje jsou v pořádku,
+    tak může být potřeba zaregistrovat IP adresy Celus.one u vašeho poskytovatele.
+    Jsou to: <ul><li>142.93.169.41</li>2a03:b0c0:3:e0::465:c001 (pro IPv6)<li></li></ul>
+  step4: Testování vašich přístupových údajů
+  step4_text:
+    Po zadání vašich přístupových údajů do Celusu Vám doporučujeme je hned otestovat.
+    Toho lze dosáhnout kliknutím na tlačítko <strong>Uložit a otestovat stahování</strong>
+    v dialogu pro editaci přístupových údajů.
+    Vyberte měsíc v blízké minulosti a zkuste stáhnout data.
+    Uvidíte ukazatel průběhu pro každé stahování a velice rychle získáte odpověd od serveru.
+    Pokud se vyskytne nějaký problém, zkontrolujte prosím údaje, které jste zadali do dialogu
+    pro editaci.
+  step5: Povolit automatické stahování
+  step5_text: |
+    Pokud jste již přístupové údaje řádně otestovali, doporučujeme Vám aktivovat automatické
+    stahování pomocí volby <strong>Automatické stahování</strong>.
+    To způsobí, že Celus se bude snažit pro dané přístupové údaje automaticky stahovat data
+    z aktuálně uzavřeného měsíce.
+  step5_text_2: |
+    <em>Pozn.</em>: Berte prosím v potaz, že Celus nezačne stahovat data hned, ale naplánuje
+    toto periodické stahování během nočních hodin, kdy je menší provoz.
+    Počkejte proto prosím den nebo dva, než se Vaše data stáhnou.
+  step6: Poďme na to.
+  step6_text:
+    OK, teď byste měli vědět vše, co je na záčátek potřeba. Klikněte na tlačítko dole
+    a přidejte Vaše první přístupové údaje do SUSHI. Hodně štěstí ;)
+  add_credentials: Přidat přístupové údaje
+  continue: Dále
 </i18n>
 <template>
   <v-stepper v-model="step" non-linear>
