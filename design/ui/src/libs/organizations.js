7ff91b282c925cd9ab699d89ecbeed416ca00da3
Stepan Henek <stepan@bigdigdata.com>
2021-12-23 10:59:59 +0100

    Alter organization name conversions in UI
    1) current language mutation
    2) `name` default language
    3) any other language mutation `name_*`
    4) `internal_id`
    5) `short_name`
    6) "-" character

--- design/ui/src/libs/organizations.js
+++ design/ui/src/libs/organizations.js
@@ -5,18 +5,27 @@ function sortOrganizations(organizations, lang) {
   let out = Object.values(organizations);
   if (out.length === 0) return [];
   let loc_name = lang ? `name_${lang}` : "name";
-  out.forEach(
-    (item) =>
-      (item["name"] = item[loc_name]
-        ? item[loc_name]
-        : item.name_cs
-        ? item.name_cs
-        : item.internal_id
-        ? item.internal_id
-        : item.short_name
-        ? item.short_name
-        : "-")
-  );
+  out.forEach((item) => {
+    if (item[loc_name]) {
+      item.name = item[loc_name]; // name based on current locale
+    } else if (item.name) {
+      // item.name should be default language
+    } else {
+      // other language mutations
+      let available = Object.entries(item)
+        .filter(([key, value]) => key.startsWith("name_") && !!value)
+        .map(([, value]) => value);
+      if (available.length > 0) {
+        item.name = available[0];
+      } else if (item.internal_id) {
+        item.name = item.internal_id; // internal_id fallback
+      } else if (item.short_name) {
+        item.name = item.short_name; // short_name fallback
+      } else {
+        item.name = "-";
+      }
+    }
+  });
   out.sort((a, b) => {
     if ("extra" in a) {
       return -1;
