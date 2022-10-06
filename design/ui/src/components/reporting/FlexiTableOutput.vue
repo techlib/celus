297d9eaefcc04eae8b0aaef624375e13b436bc2a
Beda Kosata <beda@bigdigdata.com>
2022-10-06 15:15:03 +0200

    fix: reporting - reset pagination when report changes

--- design/ui/src/components/reporting/FlexiTableOutput.vue
+++ design/ui/src/components/reporting/FlexiTableOutput.vue
@@ -341,6 +341,7 @@ export default {
         this.data = [];
         this.cleanData = [];
         this.setOrdering(report);
+        this.options.page = 1;
       }
       await this.fetchData();
     },
