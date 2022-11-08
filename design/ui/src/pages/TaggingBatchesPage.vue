6f049755cc6d2bf05a544aaf438dfb1b03a45d1a
Beda Kosata <beda@bigdigdata.com>
2022-11-08 16:53:29 +0100

    fix: add introductory text to tagging-batch list page - fixes #648

--- design/ui/src/pages/TaggingBatchesPage.vue
+++ design/ui/src/pages/TaggingBatchesPage.vue
@@ -1,10 +1,35 @@
 <i18n lang="yaml" src="@/locales/common.yaml"></i18n>
+<i18n lang="yaml">
+en:
+  desc1: |
+    To apply one tag to many titles at once, you can upload a file identifying the titles.
+    The file should be a table in CSV format which should contain at least one column with a title identifier.
+    Recognized identifiers are ISSN, eISSN and ISBN, so at least one column should have one of these names.
+  desc2: |
+    After you upload the file, it will be pre-processed and you will be able to review the result.
+    You can then apply one selected tag to the matched titles. If necessary, you can revert this action later.
+
+cs:
+  desc1: |
+    Pro aplikaci jednoho štítku na mnoho titulů najednou můžete nahrát soubor, který identifikuje tituly.
+    Soubor by měl obsahovat tabulku ve formátu CSV, který by měl obsahovat alespoň jeden sloupec s identifikátorem titulu.
+    Podporované identifikátory jsou ISSN, eISSN a ISBN, takže alespoň jeden sloupec by měl mít jeden z těchto názvů.
+  desc2: |
+    Po nahrání souboru bude provedeno předzpracování a budete moci zkontrolovat výsledek.
+    Poté můžete na nalezené tituly aplikovat zvolený štítek. Tuto akci můžete případně později vrátit zpět.
+</i18n>
 
 <template>
   <v-container fluid>
     <v-row>
       <v-col>
         <h2>{{ $t("pages.title_lists") }}</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p>{{ $t("desc1") }}</p>
+        <p>{{ $t("desc2") }}</p>
       </v-col>
     </v-row>
     <v-row>
