6c7a18047e45e7322e7187ae5c3370fdb753d8e6
Beda Kosata <beda@bigdigdata.com>
2023-01-03 10:19:07 +0100

    fix: reduce the memory requirements of deleting unused titles by deleting in batches

--- apps/publications/management/commands/remove_unused_titles.py
+++ apps/publications/management/commands/remove_unused_titles.py
@@ -1,6 +1,7 @@
+from itertools import islice
+
 from django.core.management.base import BaseCommand
 from django.db.models import Exists, OuterRef
-from django.db.transaction import atomic
 from logs.models import AccessLog
 from publications.models import Title
 
@@ -15,7 +16,6 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--do-it', dest='do_it', action='store_true')
 
-    @atomic
     def handle(self, *args, **options):
         pretend = not options['do_it']
         related_als = AccessLog.objects.filter(target_id=OuterRef('pk'))
@@ -25,6 +25,19 @@ class Command(BaseCommand):
             self.stdout.write(f'There are {qs.count()} titles to remove from {total}.')
             self.stderr.write('Just counting, use --do-it to really perform delete\n')
         else:
-            result = qs.delete()
+            # because deleting many thousands of objects at once takes a lot of memory
+            # by the postgres process (not sure why, probably it is about checking all the
+            # foreign key constraints), we delete in batches.
+            # The batch size was determined by trial and error to give reasonable speed while
+            # maintaining a reasonable memory footprint - about 500 MB in tests.
+            title_ids = list(qs.values_list('pk', flat=True))
+            self.stdout.write(f'Deleting {len(title_ids)} titles')
+            i = 0
+            batch_size = 2_000
+            iterator = iter(title_ids)
+            while batch := list(islice(iterator, batch_size)):
+                _deletes, stats = Title.objects.filter(pk__in=batch).delete()
+                i += stats['publications.Title']
+                self.stderr.write(f'Deleting {i}')
             total = Title.objects.count()
-            self.stdout.write(f'Removed the following: {result}; Remaining titles: {total}')
+            self.stdout.write(f'Removed: {i}; Remaining titles: {total}')
