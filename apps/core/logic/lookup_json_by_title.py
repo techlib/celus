be81cd68b3c4b67731c974067018cda2bbd0283e
Jakub Smetana <jakub@smetana.ml>
2023-05-23 10:25:08 +0200

    feature: add lookup_json_by_title command to print title related usage from individual JSON files

---
+++ apps/core/logic/lookup_json_by_title.py
@@ -0,0 +1,30 @@
+import json
+
+from logs.models import AccessLog
+from publications.models import Title
+from sushi.models import SushiFetchAttempt
+
+
+def lookup_json_by_title(title_id):
+    title = ' '.join(Title.objects.values_list("name", flat=True).get(id=title_id).lower().split())
+    batches = (
+        AccessLog.objects.filter(target_id=title_id)
+        .distinct()
+        .values_list('import_batch', flat=True)
+    )
+    files = SushiFetchAttempt.objects.filter(import_batch__in=batches).only('data_file')
+    out = {}
+    err = []
+    for file in files:
+        try:
+            data = json.load(file.data_file)
+        except Exception as e:
+            err.append((file.data_file.name, e))
+        else:
+            if file.data_file.name in out:
+                continue
+            out[file.data_file.name] = []
+            for item in data["Report_Items"]:
+                if ' '.join(item["Title"].lower().split()) == title:
+                    out[file.data_file.name].append(item)
+    return out, err
---
+++ apps/core/management/commands/lookup_json_by_title.py
@@ -0,0 +1,50 @@
+import json
+
+from core.logic.lookup_json_by_title import lookup_json_by_title
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = (
+        'Search through all files related to a specific title and print corresponding parts of '
+        'the json to stdout'
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'title_id',
+            help='Title id from publications/title (Title/Database)',
+        )
+        parser.add_argument(
+            '--json',
+            help='Print only valid json',
+            action='store_true',
+            dest='json',
+        )
+        parser.add_argument(
+            '--indent',
+            help='Number of spaces to indent output with (0 is no indent)',
+            dest='indent',
+            type=int,
+            default=2,
+        )
+
+    def handle(self, *args, **options):
+        indent = options['indent']
+        indent = indent if indent > 0 else None
+
+        lookup, err = lookup_json_by_title(options['title_id'])
+
+        if options['json']:
+            self.stdout.write(json.dumps(lookup, indent=indent, ensure_ascii=False))
+        else:
+            for file, items in lookup.items():
+                self.stdout.write()
+                self.stdout.write(self.style.WARNING(file))
+                for d in items:
+                    self.stdout.write(json.dumps(d, indent=indent, ensure_ascii=False)[2:-1])
+                self.stdout.write()
+
+        for file, e in err:
+            self.stderr.write(self.style.WARNING(file))
+            self.stderr.write(self.style.ERROR("  Exception reading file: " + str(e)))
