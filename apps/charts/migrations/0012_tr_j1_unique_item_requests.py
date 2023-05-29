7864c6a9aee3c6a06a314d5a525fa4f28337c66b
Stepan Henek <stepan@bigdigdata.com>
2023-05-29 21:41:15 +0200

    feature: add Unique_Item_Requests to TR_J1 ReportDataView

---
+++ apps/charts/migrations/0012_tr_j1_unique_item_requests.py
@@ -0,0 +1,23 @@
+import logging
+
+from django.db import migrations
+
+logger = logging.getLogger(__name__)
+
+
+def update_tr_j1_view(apps, schema_editor):
+    ReportDataView = apps.get_model('charts', 'ReportDataView')
+    if rdv := ReportDataView.objects.filter(short_name__iexact="TR_JR1").last():
+        rdv.metric_allowed_values = ['Total_Item_Requests', 'Unique_Item_Requests']
+        rdv.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('charts', '0011_remove_reportdataview_primary_dimension'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_tr_j1_view, migrations.RunPython.noop),
+    ]
