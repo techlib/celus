3b79d98249e1afc111b4b2d15a9424dfa5670374
Stepan Henek <stepan@bigdigdata.com>
2023-06-21 13:27:15 +0200

    feature: debug celery using celery.contrib.rdb

--- start_celery.sh
+++ start_celery.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export PYTHONBREAKPOINT=celery.contrib.rdb.set_trace
 watchmedo auto-restart -d apps/ -d config/  -p '*.py' -R -- celery -A config worker -Q export,celery,interest,sushi,import,normal,preflight -l DEBUG
