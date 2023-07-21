9da25e2612bd400d568d3533d3734cae94cead94
Stepan Henek <stepan@bigdigdata.com>
2023-07-21 10:26:11 +0200

    feature: brain API changes - added available_metrics to parser definitions

--- apps/knowledgebase/serializers.py
+++ apps/knowledgebase/serializers.py
@@ -53,6 +53,7 @@ class ParserDefinitionSerializer(serializers.Serializer):
     titles_to_skip = serializers.JSONField(required=True)
     dimensions_to_skip = serializers.JSONField(required=True)
     metric_aliases = serializers.JSONField(required=True)
+    available_metrics = serializers.JSONField(required=True, allow_null=True)
     dimension_aliases = serializers.JSONField(required=True)
     heuristics = serializers.JSONField(required=True)
     possible_row_offsets = serializers.JSONField(required=True)
