53afe60310a54c30fecb177df4f84a8028e635fa
Stepan Henek <stepan@bigdigdata.com>
2023-08-01 16:06:39 +0200

    feature(ui): ease url validation for sushi credentials

--- design/ui/specs/sushi-validation.spec.js
+++ design/ui/specs/sushi-validation.spec.js
@@ -12,6 +12,22 @@ let cases = [
   ["https://reports.b.c/report/TR", true],
   ["https://a.reports.b.c/foobar", true],
   ["reports/b.c/foobar", true],
+  ["https://a.b.c/reports/c5/", true],
+  ["https://a.b.c/reports/tr/sub", true],
+  ["https://a.b.c/reports/tr/", false],
+  ["https://a.b.c/reports/ir_m1/", false],
+  ["https://a.b.c/reports/ir/", false],
+  ["https://a.b.c/reports/ir_m1", false],
+  ["https://a.b.c/reports/ir", false],
+  ["https://a.b.c/subpath/reports/c5/", true],
+  ["https://a.b.c/subpath/reports/tr/sub", true],
+  ["https://a.b.c/subpath/reports/tr/", false],
+  ["https://a.b.c/subpath/reports/ir_m1/", false],
+  ["https://a.b.c/subpath/reports/ir_m1", false],
+  ["https://a.b.c/subpath/reports/ir", false],
+  ["https://docuseek2.com/reports/counter5/reports/pr", false], // real-world example
+  ["https://docuseek2.com/reports/counter5/", true], // real-world example
+  ["https://docuseek2.com/reports/counter5_1/", true],
 ];
 
 describe("test SUSHI URL validation", () => {
--- design/ui/src/libs/sushi-validation.js
+++ design/ui/src/libs/sushi-validation.js
@@ -1,4 +1,13 @@
-const reportRegexp = new RegExp("/reports($|\\W)");
+// should match urls that end like this
+// /report
+// /report/
+// /report/tr
+// /report/tr/
+// /report/ir_m1
+// /report/ir_m1/
+const reportRegexp = new RegExp(
+  "/reports(/*$|/[Tt][Rr]|/[Dd][Rr]|/[Pp][Rr]|/[Ii][Rr])(|_[^/]+){0,1}/{0,1}$"
+);
 
 function testSushiUrlReport(url) {
   // returns true if the url is ok, false otherwise
