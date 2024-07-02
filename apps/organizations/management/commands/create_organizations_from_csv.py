7ce2e0204d86476273460093af88060a00ba09d9
Beda Kosata <beda@bigdigdata.com>
2024-07-02 11:10:38 +0200

    update create_organizations_from_csv.py to work correctly from the UI

--- apps/organizations/management/commands/create_organizations_from_csv.py
+++ apps/organizations/management/commands/create_organizations_from_csv.py
@@ -1,5 +1,7 @@
 import logging
+from argparse import FileType
 from collections import Counter
+from io import BufferedReader
 
 from django.core.management.base import BaseCommand
 from django.db.transaction import atomic
@@ -14,31 +16,31 @@ class Command(BaseCommand):
     help = "Create/sync organizations with CSV file"
 
     def add_arguments(self, parser):
-        parser.add_argument("csv_file")
+        parser.add_argument("-f", dest="csv_file", type=FileType("rb"))
         parser.add_argument("--do-it", dest="doit", action="store_true")
 
     @atomic
     def handle(self, *args, **options):
         stats = Counter()
-        with open(options["csv_file"], "r") as infile:
-            reader = get_dict_reader_from_csv(infile)
-            for row in reader:
-                name = row.get("name")
-                if not name:
-                    raise ValueError('Row "name" is required')
-                name = name.strip()
-                short_name = row.get("short_name", name)
-                org_params = {"short_name": short_name}
-                for extra_attr in ["name_cs", "ext_id", "internal_id"]:
-                    if ext_value := row.get(extra_attr):
-                        org_params[extra_attr] = ext_value
-                _, created = Organization.objects.update_or_create(defaults=org_params, name=name)
-                if created:
-                    stats["organization_created"] += 1
-                    logger.debug("created organization %s: %s", name, org_params)
-                else:
-                    stats["organization_existed"] += 1
-                    logger.debug("updating organization %s: %s", name, org_params)
+        # get_dict_reader_from_csv does not support InMemoryUploadedFile directly, so we wrap it
+        reader = get_dict_reader_from_csv(BufferedReader(options["csv_file"]))
+        for row in reader:
+            name = row.get("name")
+            if not name:
+                raise ValueError('Row "name" is required')
+            name = name.strip()
+            short_name = row.get("short_name", name)
+            org_params = {"short_name": short_name}
+            for extra_attr in ["name_cs", "ext_id", "internal_id"]:
+                if ext_value := row.get(extra_attr):
+                    org_params[extra_attr] = ext_value
+            _, created = Organization.objects.update_or_create(defaults=org_params, name=name)
+            if created:
+                stats["organization_created"] += 1
+                logger.debug("created organization %s: %s", name, org_params)
+            else:
+                stats["organization_existed"] += 1
+                logger.debug("updating organization %s: %s", name, org_params)
 
         self.stderr.write(self.style.WARNING(f"Import stats: {stats}"))
         if not options["doit"]:
