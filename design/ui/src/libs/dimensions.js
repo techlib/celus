de4af3ed4d1ceb9c84ce9bdd1f8e17921546254a
Beda Kosata <beda@bigdigdata.com>
2024-08-07 12:53:02 +0200

    fix: fix "off-by-one" error in reporting when switching report types and "dim7" is used for columns + update the number of explicit dimensions to 8

--- design/ui/src/libs/dimensions.js
+++ design/ui/src/libs/dimensions.js
@@ -1,8 +1,8 @@
-const explicitDimensionCount = 7;
+const explicitDimensionCount = 8;
 const explicitDimensions = [];
 
 for (let i = 0; i < explicitDimensionCount; i++) {
-  explicitDimensions.push(`dim${i}`);
+  explicitDimensions.push(`dim${i + 1}`);
 }
 
 export { explicitDimensions, explicitDimensionCount };
