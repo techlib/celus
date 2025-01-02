3eeb1abf706ad5cdd6e77546e52e9c6a3bd208ae
Beda Kosata <beda@bigdigdata.com>
2025-01-02 08:02:55 +0100

    fix: when running remove_orphaned_files all files under media/counter for deleting - not only those in dirs with existing attempts

--- apps/sushi/management/commands/remove_orphaned_files.py
+++ apps/sushi/management/commands/remove_orphaned_files.py
@@ -1,11 +1,10 @@
 import logging
-import sys
-import tempfile
+import os
 import time
 from collections import Counter
 from pathlib import Path
 
-import diskcache
+from core.logic.debug import log_memory
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
@@ -28,6 +27,9 @@ class Command(BaseCommand):
             type=int,
             default="30",
         )
+        parser.add_argument(
+            "-p", "--print-deleted", action="store_true", help="Print deleted files"
+        )
 
     def handle(self, *args, **options):
         if options["older_than"] < 1:
@@ -35,40 +37,41 @@ class Command(BaseCommand):
         time_limit = time.time() - options["older_than"] * 24 * 60 * 60
 
         stats = Counter()
-        dirs = set()  # Note: We assume here that directory list will fit into memory
-        with tempfile.TemporaryDirectory(prefix="celus") as tmp:
-            cache = diskcache.Cache(tmp)
-            for attempt in SushiFetchAttempt.objects.filter(data_file__isnull=False).exclude(
-                data_file__exact=""
-            ):
-                filepath = Path(settings.MEDIA_ROOT) / attempt.data_file.name
-                if filepath.exists():
-                    stats["existing_attempt_files"] += 1
-                else:
-                    stats["missing_attempt_files"] += 1
-                    continue
+        deleted_size = 0
+        cache = set()
 
-                cache.add(str(filepath), "1", expire=None)  # never expires
+        for filename in (
+            SushiFetchAttempt.objects.filter(data_file__isnull=False)
+            .exclude(data_file__exact="")
+            .values_list("data_file", flat=True)
+        ):
+            filepath = Path(settings.MEDIA_ROOT) / filename
+            if filepath.exists():
+                stats["existing_attempt_files"] += 1
+            else:
+                stats["missing_attempt_files"] += 1
+                continue
 
-                parent = filepath.parent
-                if parent not in dirs:
-                    stats["dirs"] += 1
+            cache.add(str(filepath))
 
-                dirs.add(parent)
+        log_memory("Before cleaning dirs")
+        counter_root = Path(settings.MEDIA_ROOT) / "counter"
+        for root, _dirnames, filenames in os.walk(str(counter_root)):
+            for filename in filenames:
+                fullpath = Path(root) / filename
+                if (
+                    fullpath.is_file()
+                    and fullpath.stat().st_mtime < time_limit
+                    and str(fullpath) not in cache
+                ):
+                    stats["deleted_orphan_files"] += 1
+                    deleted_size += fullpath.stat().st_size
+                    if options["doit"]:
+                        fullpath.unlink()
+                    if options["print_deleted"]:
+                        print(fullpath)
 
-            dirs_len = len(dirs)
-            print()
-            for idx, dir_path in enumerate(dirs):
-                sys.stdout.write(f"\rCleaning dirs {idx + 1}/{dirs_len}")
-
-                for entry in dir_path.iterdir():
-                    if entry.is_file() and entry.stat().st_mtime < time_limit:
-                        if cache.get(str(entry), "0") == "0":
-                            stats["deleted_orphan_files"] += 1
-                            if options["doit"]:
-                                entry.unlink()
-            print("\n")
-
-        print(stats)
+        logger.info("Stats: %s", stats)
+        logger.info("Deleted size: %.2f MB", deleted_size / 1024 / 1024)
         if not options["doit"]:
-            print("Files were really not deleted. --do-it to really do it ;)")
+            logger.warning("Files were really not deleted. --do-it to really do it ;)")
