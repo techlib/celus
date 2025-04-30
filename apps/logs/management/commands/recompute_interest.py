13c061af6b8d25b556bcf342622dbb5d14f3ff93
Beda Kosata <beda@bigdigdata.com>
2025-04-30 08:39:07 +0200

    modify recompute_interest.py not to use atomic - transaction seems to cause problems for large databases when it is committed

--- apps/logs/management/commands/recompute_interest.py
+++ apps/logs/management/commands/recompute_interest.py
@@ -1,13 +1,50 @@
 import logging
+from collections import Counter
+from datetime import timedelta
 from time import monotonic
 
 from django.core.management.base import BaseCommand
-from django.db.transaction import atomic
 
-from logs.logic.interest.computation import recompute_interest_by_batch
-from logs.models import ImportBatch
+from logs.logic.interest.computation import sync_interest_for_import_batch
+from logs.models import ImportBatch, ReportType
 
 logger = logging.getLogger(__name__)
+
+
+def recompute_interest_by_batch(queryset=None):
+    """
+    This is a private copy of `recompute_interest_by_batch` from `logic.interest.computation`.
+    It is slighly simplified and does not use import batch locking. It is indended for cases
+    when we are sure that no other recomputations are happening - usually because celery etc.
+    are switched off.
+
+    The reason why locking is not used here is that with locking postgres takes loooong time
+    to do the processing after the lock is released and transaction is committed. It also eats
+    up a lot of memory during that time.
+    """
+    if queryset is None:
+        queryset = ImportBatch.objects.filter(interest_timestamp__isnull=False)
+    stats = Counter()
+    total_count = queryset.count()
+    logger.info("Going to recompute interest for %d batches", total_count)
+    if total_count == 0:
+        return stats
+    interest_rt = ReportType.objects.get_interest_rt()
+    start = monotonic()
+    for i, import_batch in enumerate(queryset.iterator()):
+        stats += sync_interest_for_import_batch(import_batch, interest_rt)
+        if i and i % 100 == 0:
+            logger.info(
+                "Recomputed interest for %d out of %d batches (%.1f%%), elapsed: %s, ETA: %s, "
+                "stats: %s",
+                i,
+                total_count,
+                i * 100 / total_count,
+                timedelta(seconds=monotonic() - start),
+                timedelta(seconds=(monotonic() - start) / (i + 1) * (total_count - i)),
+                stats,
+            )
+    return stats
 
 
 class Command(BaseCommand):
@@ -19,7 +56,6 @@ class Command(BaseCommand):
             "-r", dest="report_type", help="short name of the report_type to process"
         )
         parser.add_argument("-f", dest="force", action="store_true", help="force recomputation")
-        parser.add_argument("--verbose", dest="verbose", action="store_true")
 
     def handle(self, *args, **options):
         filters = {}
@@ -31,10 +67,10 @@ class Command(BaseCommand):
         start = monotonic()
         ib_count = qs.count()
         logger.info("Going to recompute interest for %d ImportBatches", ib_count)
-        with atomic():
-            if options["force"]:
-                qs.update(interest_timestamp=None)
-            stats = recompute_interest_by_batch(qs, verbose=options["verbose"])
+
+        if options["force"]:
+            qs.update(interest_timestamp=None)
+        stats = recompute_interest_by_batch(qs)
 
         logger.info("Duration: %s, Stats: %s", monotonic() - start, stats)
         logger.info(
