b5b96943b2e6d692c091586e7b9ce93a7387960a
DianaVarsikova <d.varsikova@email.cz>
2025-09-02 10:05:24 +0200

    feature: add support for typescript in Vue components

--- design/ui/.eslintrc.js
+++ design/ui/.eslintrc.js
@@ -9,4 +9,23 @@ module.exports = {
     "plugin:vue/base",
     //"plugin:vuetify/base",
   ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.vue"],
+      parser: "vue-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".vue"],
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+      rules: {},
+    },
+  ],
 };
