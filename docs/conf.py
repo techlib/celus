d105e89d9e9b5a990f8a9dd69d47939fe13ddea0
Beda Kosata <beda@bigdigdata.com>
2025-11-27 14:48:18 +0100

    bump version to 11.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = f"{date.today().year}, BigDigData"
 author = "Beda Kosata"
 
 # The full version, including alpha/beta/rc tags
-release = "11.0.0"
+release = "11.0.1"
 
 
 # -- General configuration ---------------------------------------------------
