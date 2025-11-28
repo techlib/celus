d5c612cd7a5314d53ac1955e7767e27dba1cff50
Beda Kosata <beda@bigdigdata.com>
2025-11-28 13:48:50 +0100

    export DJANGO_SETTINGS_MODULE in development celery and celerybeat scripts

--- start_celery.sh
+++ start_celery.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export PYTHONBREAKPOINT=celery.contrib.rdb.set_trace
+export DJANGO_SETTINGS_MODULE=config.settings.devel
 # watchmedo auto-restart -d apps/ -d config/  -p '*.py' -R -- celery -A config worker -Q export,celery,interest,sushi,import,normal,preflight -l DEBUG
 uv run celery -A config worker -Q export,celery,interest,sushi,import,normal,preflight,ch_export --concurrency=1 -l DEBUG
--- start_celerybeat.sh
+++ start_celerybeat.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export DJANGO_SETTINGS_MODULE=config.settings.devel
 watchmedo auto-restart -d apps/ -d config/  -p '*.py' -R -- celery -A config beat -l DEBUG
